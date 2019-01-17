fa9245cb84a73f2fe30efa48aa02ced8815e99b8
wolfpan <haoyupan@aliyun.com>
2019-01-18 02:10:14 +0800

    Update doc

--- docs/conf.py
+++ docs/conf.py
@@ -1,2 +1,4 @@
 source_suffix = '.md'
 master_doc = 'index'
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.pngmath',
+              'sphinx.ext.mathjax', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode']
