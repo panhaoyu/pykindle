74ac2b3a0067703d9b5ce621420581395774f6d0
wolfpan <haoyupan@aliyun.com>
2019-01-18 02:13:36 +0800

    Update doc

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
-import commonmark
-
+import recommonmark.parser
 
 source_suffix = '.md'
+source_parsers = {'.md': recommonmark.parser.CommonMarkParser}
 master_doc = 'index'
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo',
               'sphinx.ext.mathjax', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode']
