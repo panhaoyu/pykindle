6e1a47eb29b5640689764315dcafb84205099105
wolfpan <haoyupan@aliyun.com>
2019-01-19 03:30:27 +0800

    Include package data, to fix the bug that templates are not in python package.

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
     install_requires=['jinja2', 'markdown'],
     url="https://github.com/panhaoyu/pykindle",
     packages=find_packages(),
+    include_package_data=True,
     license='GPL',
     classifiers=[
         'Intended Audience :: Developers',
