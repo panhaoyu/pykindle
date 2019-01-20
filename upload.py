8be207f0155d153475f6d42852626506a02e4f8b
wolfpan <haoyupan@aliyun.com>
2019-01-20 12:13:01 +0800

    change upload.py, delete the output dirs after upload.

--- upload.py
+++ upload.py
@@ -6,8 +6,6 @@ print('自动检测到的依赖包：')
 [print('\t', package) for package in setuptools.find_packages()]
 print()
 
-if os.path.exists('dist'):
-    shutil.rmtree('dist')
 os.system('python setup.py sdist bdist_wheel')
 os.system('twine upload dist/*')
 shutil.rmtree('dist')
