89e67a8850dec31a06db613bbd56b921b67dc7da
wolfpan <haoyupan@aliyun.com>
2019-01-20 10:44:07 +0800

    test docs.

--- docs/conf.py
+++ docs/conf.py
@@ -197,8 +197,11 @@ todo_include_todos = True
 def setup(app):
     app.add_config_value('recommonmark_config', {
         # 'url_resolver': lambda url: github_doc_root + url,
+        'enable_auto_toc_tree': True,
         'auto_toc_tree_section': 'Contents',
+        'enable_math': True,
+        'enable_inline_math': True,
         'enable_eval_rst': True,
-        'enable_auto_doc_ref': True,
+        # 'enable_auto_doc_ref': True,
     }, True)
     app.add_transform(recommonmark.transform.AutoStructify)
